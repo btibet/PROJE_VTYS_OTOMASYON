257665ab1dde8769b033856e85649db9ae70ba1a
Tibet Ayaydın <btibetxx@gmail.com>
2021-01-06 19:15:58 +0300

    Form1 için arayüz kodları girildi

--- PROJE_VTYS_OTOMASYON/Form1.cs
+++ PROJE_VTYS_OTOMASYON/Form1.cs
@@ -15,6 +15,7 @@ namespace PROJE_VTYS_OTOMASYON
         public Form1()
         {
             InitializeComponent();
+ 
         }
         private void Form1_Load(object sender, EventArgs e)
         {
